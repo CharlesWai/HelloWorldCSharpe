71983f0d8c126eb0d25ce9d95d97dc4864867b84
CharlesWai <w409201574@gmail.com>
2023-04-05 18:03:30 +0800

    新增打招呼

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloWorld.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Person person = new Person() {Name ="CharlesWai", Age = 27 };
+            Console.WriteLine($"Hello {person.Name}, you are {person.Age} years old");
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
