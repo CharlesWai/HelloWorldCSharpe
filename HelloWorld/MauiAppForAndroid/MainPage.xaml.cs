8d17b2c68fdf328e4ff92ccbda4f1628d0898a14
CharlesWai <w409201574@gmail.com>
2023-04-13 00:43:15 +0800

    Add VibrationEffect when the button clicked

--- HelloWorld/MauiAppForAndroid/MainPage.xaml.cs
+++ HelloWorld/MauiAppForAndroid/MainPage.xaml.cs
@@ -1,18 +1,27 @@
-﻿namespace MauiAppForAndroid
+﻿using Xamarin.Android;
+using Xamarin.Kotlin;
+using Android.OS;
+using Android.Content;
+
+namespace MauiAppForAndroid
 {
     public partial class MainPage : ContentPage
     {
+        private Vibrator vibrator;
         int count = 0;
 
         public MainPage()
         {
+            this.BindingContext = this;
             InitializeComponent();
+            vibrator = Vibrator.FromContext((Context)this.BindingContext);
+            
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
             count++;
-
+            vibrator.Vibrate(VibrationEffect.EffectClick);
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
             else
